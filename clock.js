25ee22b69464e7c3e10db14066e4dac057aea06e
nina <32724947+ninalv@users.noreply.github.com>
2017-10-13 17:38:52 +0900

    Remove seconds

--- clock.js
+++ clock.js
@@ -10,16 +10,16 @@ function clock()
 	var w = weeks[now.getDay()];
 	var h = now.getHours();
 	var mi = now.getMinutes();
-	var s = now.getSeconds();
+	//var s = now.getSeconds();
 
 	if (mo < 10) mo = "0" + mo;
 	if (d < 10) d = "0" + d;
 	if (mi < 10) mi = "0" + mi;
-	if (s < 10) s = "0" + s;
+	//if (s < 10) s = "0" + s;
 
 	document.getElementById("clock_date").innerHTML =  y + "/" + mo + "/" + d + " (" + w + ")";
 	document.getElementById("clock_time").innerHTML = h + ":" + mi
-		+ ":" + s
+		//+ ":" + s
 	;
 	document.getElementById("clock_frame").style.fontSize =  window.innerWidth / 10 + "px";
 }
